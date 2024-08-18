9d951d29bf30297b5b669dbe17fccc8ea251f77f
SamCorbett <Samcorbett1994@gmail.com>
2024-08-18 08:47:22 -0500

    Add files via upload

---
+++ moon.js
@@ -0,0 +1,72 @@
+//MOON ASCII
+let fullMoon = "<pre><br>"+
+"       _,,,_     <br>"+
+"     .:::::::.  <br>"+
+"    :::::::::::   FULL MOON <br>"+
+"    :::#:::O::: <br>"+
+"    `:##::::::' <br>"+
+"      `':::''  <br>"+
+"</pre>";
+
+let waningGibbous = "<pre><br>"+
+"       _,,,_    <br>"+
+"     .::::. `.  <br>"+
+"    :::::::O  :    WANING GIBBOUS<br>"+
+"    :::#::::  :  <br>"+
+"    `:##:::' .'  <br>"+
+"      `'::'-'   <br>"+
+"</pre>";
+
+let lastQuarter = "<pre><br>"+
+"       _,,._   <br>"+
+"     .::::  `.  <br>"+
+"    :::::: o  :    LAST QUARTER <br>"+
+"    :::#::    : <br>"+
+"    ':##::   .' <br>"+
+"      `'::.-' <br>"+
+"</pre>";
+
+let waningCrescent = "<pre><br>"+
+"       _,.._     <br>"+
+"     .::'         <br>"+
+"    :::            WANING CRESCENT <br>"+
+"    :::           <br>"+
+"    ':#.         <br>"+
+"      `':..-     <br>"+
+"</pre>";
+
+let newMoon = "<pre><br>"+
+"       _..._     <br>"+
+"     .'     `.    <br>"+
+"    :         :    NEW MOON <br>"+
+"    :         :   <br>"+
+"    '.       .'  <br>"+
+"      `-...-'    <br>"+
+"</pre>";
+
+let waxingCrescent = "<pre><br>"+
+"        ..,_     <br>"+
+"          `::.    <br>"+
+"           o:::    WAXING CRESCENT <br>"+
+"            :::  <br>"+
+"           .::'  <br>"+
+"       -..:''    <br>"+
+"</pre>";
+
+let firstQuarter = "<pre><br>"+
+"       _.,,_     <br>"+
+"     .'  >:::.    <br>"+
+"    :    >:O:::    FIRST QUARTER <br>"+
+"    :    >:::::  <br>"+
+"    '.   :::::'  <br>"+
+"      `-.::''    <br>"+
+"</pre>";
+
+let waxingGibbous = "<pre><br>"+
+"       _.,,_     <br>"+
+"     .' .::::.    <br>"+
+"    :  ::::O:::    WAXING GIBBOUS <br>"+
+"    :  ::::::::  <br>"+
+"    '. '::::::'  <br>"+
+"      `-.::''    <br>"+
+"</pre>";
